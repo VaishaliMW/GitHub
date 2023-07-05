573e748b03971fdd895214f6154db2f4df07e961
vaishali <vaishaliwadaskar12@gmail.com>
2023-07-05 13:07:51 +0530

    This is first commit

---
+++ src/test/java/Day1/HTTPRequest.java
@@ -0,0 +1,18 @@
+package Day1;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matcher.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class HTTPRequest {
+	@Test
+	void getUser() {
+		given().
+		
+	}
+
+}
